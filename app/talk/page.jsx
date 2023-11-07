4af9287b34857fdd0f80eaba9597757872768421
Joseph <Josephmburumungai99@gmail.com>
2023-11-07 07:14:02 +0300

    Created different pages and  components

---
+++ app/talk/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Lets Talk page</div>
+  )
+}
+
+export default page
---
+++ app/testimonials/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Testimonial page</div>
+  )
+}
+
+export default page
